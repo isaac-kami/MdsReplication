5565b785fc00924d918e88ebfb5cfd91253485cb
Zack <76065975+isaac-kami@users.noreply.github.com>
2021-04-20 17:30:40 +0300

    ..

--- service_gateway.tf
+++ service_gateway.tf
@@ -19,21 +19,21 @@ resource "oci_core_service_gateway" "bucket_SVG" {
     display_name = "mysql service gateway"
 }
 
-resource "oci_core_route_table" "examplebucket_route" {
+#resource "oci_core_route_table" "examplebucket_route" {
 
-    compartment_id = oci_identity_compartment.MySqlOciCompartment.id
-    vcn_id = oci_core_virtual_network.MySqlOciVCN.id
+    #compartment_id = oci_identity_compartment.MySqlOciCompartment.id
+   # vcn_id = oci_core_virtual_network.MySqlOciVCN.id
     
     
-   route_rules {
-          cidr_block        = "0.0.0.0/0"
-          network_entity_id = oci_core_internet_gateway.MySqlOciInternetGateway.id
-       }
+ #  route_rules {
+ #        cidr_block        = "0.0.0.0/0"
+ #        network_entity_id = oci_core_internet_gateway.MySqlOciInternetGateway.id
+ #    }
 
-   route_rules    {
-        destination =  lookup(data.oci_core_services.mysql_services.services[1],"cidr_block")
-        destination_type = "SERVICE_CIDR_BLOCK"
-        network_entity_id = oci_core_service_gateway.bucket_SVG.id
-       }
+ #  route_rules    {
+ #       destination =  lookup(data.oci_core_services.mysql_services.services[1],"cidr_block")
+ #       destination_type = "SERVICE_CIDR_BLOCK"
+ #       network_entity_id = oci_core_service_gateway.bucket_SVG.id
+ #      }
 
-}
+#}
