12da0617edbea870deddc785c983eae83487435f
Zack <76065975+isaac-kami@users.noreply.github.com>
2021-04-20 21:10:12 +0300

    ..

--- create_bucket.tf
+++ create_bucket.tf
@@ -1,4 +1,8 @@
 resource "oci_objectstorage_bucket" "mds_bucket" {
+    
+    # do not delete depends_on
+    
+    depends_on = [oci_identity_compartment.MySqlOciCompartment]
 
     compartment_id = oci_identity_compartment.MySqlOciCompartment.id
     name = var.name_bucket
